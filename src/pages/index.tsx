ae1686b1706650bea61ba80eb84567cac5e87ea9
Matthew Hart <mattevanhart@gmail.com>
2023-08-20 02:57:17 -0400

    add three and add cube to scene

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
+import { Canvas, type ThreeElements } from "@react-three/fiber";
+
+function Cube(props: ThreeElements["mesh"]) {
+  const cube = useState(null);
+
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial color={"purple"} />
+    </mesh>
+  );
+}
 
 export default function Home() {
   return (
@@ -10,6 +23,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <Canvas>
+          <Cube position={[0, 0, 0]} />
+          <ambientLight />
+        </Canvas>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
