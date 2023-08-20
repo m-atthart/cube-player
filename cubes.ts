2cbd8be7f34847c9fa8a10b55594cf6905d79e2d
Matthew Hart <mattevanhart@gmail.com>
2023-08-20 04:22:42 -0400

    change name to mosaicube

--- cubes.ts
+++ cubes.ts
@@ -10,8 +10,6 @@ const solveCount = performAlg(
   "Z2 U L U' L F' R' D' F L' U L U R U R' U L U L' U L U' L' D2 R' U R D2 Y' R U' R' U' R U R' f R U R' U' f' U F R U R' U' F' U2 R U R' U' R' F R2 U' R' U' R U R' F'"
 );
 printCube(cube);
-console.log(`Scramble count: ${scrambleCount}`);
-console.log(`Solve count: ${solveCount}`);
 
 // const cube2 = getSolvedCube();
 // performAlg(cube2, "R U R' U' R' F R2 U' R' U' R U R' F'");
